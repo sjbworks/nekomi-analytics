da2803232b9d27e179fe311947495c067bdca495
sjbworks <sacred.jizo.bosatz@gmail.com>
2021-03-07 03:06:38 +0900

    edit app sources to apply tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from 'assets/icon-512x512.png'
+import './App.css'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default App
